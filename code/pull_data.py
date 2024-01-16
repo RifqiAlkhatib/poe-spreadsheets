d796e3e0c5564e07db437edef14f3db031bc7357
Rifqi Alkhatib <rifqialkhatib@gmail.com>
2024-01-16 22:45:20 +0800

    created function to pull currency data

--- code/pull_data.py
+++ code/pull_data.py
@@ -1,6 +1,60 @@
-import json
+from typing import TypedDict, Literal
+from dataclasses import dataclass
 
-with open("config.json", "r") as j:
+import requests
+import json
+import pandas as pd
+
+
+with open("../code/config.json", "r") as j:
     config = json.load(j)
 
-print(config['league'])
+league = config['league']
+URL = f"https://poe.ninja/api/data/currencyoverview?league={league}&type=Currency"
+
+
+def pull_currency_data(url):
+    """
+    Function to pull currency data from poe.ninja API
+    """
+    try:
+        r = requests.get(url=url)
+        return r.json()
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
+    
+
+def extract_data(currency_raw):
+    """
+    Function to extract chaos equivalent value for each currency type
+    """
+    final_data = []
+
+    for currency in currency_raw["lines"]:
+        currency_data = {
+            "currency_type": currency["currencyTypeName"],
+            "chaos_eq": currency["chaosEquivalent"]
+        }
+
+        try:
+            currency_data["chaos_sell"] = round(1 / currency["pay"]["value"], 4)
+        except:
+            currency_data["chaos_sell"] = None
+
+        try:
+            currency_data["chaos_buy"] = round(currency["receive"]["value"], 4)
+        except:
+            currency_data["chaos_buy"] = None
+
+        final_data.append(currency_data)
+
+    return pd.DataFrame(final_data)
+
+
+if __name__ == "__main__":
+    currency_raw = pull_currency_data(URL)
+    final_data = extract_data(currency_raw)
+
+    final_data.to_csv("../data/final_data.csv", index=False)
