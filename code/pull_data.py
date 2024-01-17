c5976d64ce5931a93450821b7d7fe0c768ded083
Rifqi Alkhatib <rifqialkhatib@gmail.com>
2024-01-18 00:02:35 +0800

    refactored code into dataclass

--- code/pull_data.py
+++ code/pull_data.py
@@ -1,5 +1,5 @@
-from typing import TypedDict, Literal
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Dict
 
 import requests
 import json
@@ -9,56 +9,74 @@ import pandas as pd
 with open("../code/config.json", "r") as j:
     config = json.load(j)
 
-base_api = config["API"]["base_api"]
-league = config["API"]["league"]
-URL = base_api.format(category="currency", league=league, type="Currency")
 
+@dataclass(kw_only=True)
+class PoeNinja:
+    category: str
+    base_api: str = config["API"]["base_api"]
+    league: str = config["API"]["league"]
+    api_group: str = field(init=False)
+    api_category: str = field(init=False)
+    _url: str = field(init=False)
 
-def pull_currency_data(url):
-    """
-    Function to pull currency data from poe.ninja API
-    """
-    try:
-        r = requests.get(url=url)
-        return r.json()
     
-    except Exception as e:
-        print(f"Error: {e}")
-        return {}
+    def __post_init__(self) -> None:
+        self.api_group = config["API"][self.category]["group"]
+        self.api_category = config["API"][self.category]["category"]
+        self._url = self.base_api.format(
+            group=self.api_group, league=self.league, category=self.api_category
+        )
     
 
-def extract_data(currency_raw):
-    """
-    Function to extract chaos equivalent value for each currency type
-    """
-    final_data = []
+    def _extract_data(self, raw_data: Dict[str, list]) -> pd.DataFrame:
+        final_data = []
 
-    for currency in currency_raw["lines"]:
-        currency_data = {
-            "currency_type": currency["currencyTypeName"],
-            "chaos_eq": currency["chaosEquivalent"]
-        }
+        if self.api_group == "currency":
+            name_key = "currencyTypeName"
+            value_key = "chaosEquivalent"
 
+        elif self.api_group == "item":
+            name_key = "name"
+            value_key = "chaosValue"
+        
+        else:
+            raise ValueError("API `group` can only be 'currency' or 'item'")
+
+        for item in raw_data["lines"]:
+            item_data = {
+                "name": item[name_key],
+                "chaos_value": item[value_key]
+            }
+
+            if self.api_group == "currency":
+                item_data["chaos_sell"] = item.get("pay", {}).get("value")
+                item_data["chaos_buy"] = item.get("receive", {}).get("value")
+                
+            final_data.append(item_data)
+        
+        final_df = pd.DataFrame(final_data)
+
+        if self.api_group == "currency":
+            final_df['chaos_sell'] = 1 / final_df['chaos_sell']
+
+        return round(final_df, 4)
+
+
+    def pull_data(self) -> pd.DataFrame:
         try:
-            currency_data["chaos_sell"] = round(1 / currency["pay"]["value"], 4)
-        except:
-            currency_data["chaos_sell"] = None
-
-        try:
-            currency_data["chaos_buy"] = round(currency["receive"]["value"], 4)
-        except:
-            currency_data["chaos_buy"] = None
-
-        final_data.append(currency_data)
-
-    return pd.DataFrame(final_data)
+            r = requests.get(url=self._url)
+            return self._extract_data(r.json())
+        
+        except Exception as e:
+            print(f"Error: {e}")
+            return pd.DataFrame()
 
 
-def main():
-    currency_raw = pull_currency_data(URL)
-    final_data = extract_data(currency_raw)
+def main():    
+    currency = PoeNinja(category="currency")
+    final_data = currency.pull_data()
 
-    final_data.to_csv("../data/final_data.csv", index=False)
+    final_data.to_csv("../data/_final_data.csv", index=False)
 
 
 if __name__ == "__main__":
